3edbfaa01501e68daf3275dbd00d23ce0c4e6331
Vincent <vleclerc@inow.fr>
2020-07-27 17:37:08 +0200

    add api ressource & client

---
+++ IdentityServer/IdentityServer/Controllers/ManageApiController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Mappers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityServer.Controllers
+{
+    public class ManageApiController : Controller
+    {
+        private readonly ConfigurationDbContext _confContext;
+
+        public ManageApiController(ConfigurationDbContext confContext)
+        {
+            _confContext = confContext;
+        }
+
+
+        public IActionResult AddApiRessource()
+        {
+            IdentityServer4.Models.ApiResource api = new IdentityServer4.Models.ApiResource();
+
+            api.Name = "apimeteo";
+            api.Description = "description de l'api";
+            api.DisplayName = "API meteo";
+            api.Enabled = true;
+
+
+            _confContext.ApiResources.Add(api.ToEntity()) ;
+            _confContext.SaveChanges();
+            return Ok();
+        }
+    }
+}
---
+++ IdentityServer/IdentityServer/Controllers/ManageClientController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Entities;
+using IdentityServer4.EntityFramework.Mappers;
+using IdentityServer4.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityServer.Controllers
+{
+    public class ManageClientController : Controller
+    {
+
+        private readonly ConfigurationDbContext _confContext;
+
+        public ManageClientController(ConfigurationDbContext confContext)
+        {
+            _confContext = confContext;
+        }
+
+        public IActionResult AddClient()
+        {
+            IdentityServer4.Models.Client client = new IdentityServer4.Models.Client();
+
+            client.ClientId = "mobileapp";
+            client.ClientSecrets = new List<IdentityServer4.Models.Secret> { new IdentityServer4.Models.Secret("cleduclient".Sha256()) };
+
+            client.AllowedScopes = new List<string> { "api_meteo_scope" };
+
+            _confContext.Clients.Add(client.ToEntity());
+            _confContext.SaveChanges();
+            return Ok();
+        }
+
+        public IActionResult UpdateClient()
+        {
+
+            var client = _confContext.Clients.Find(1);
+            /*var model = client.ToModel();
+
+            model.AllowedGrantTypes = GrantTypes.ClientCredentials;
+
+            client = model.ToEntity();*/
+            client.AllowedGrantTypes = new List<ClientGrantType> { new ClientGrantType { GrantType = GrantType.ClientCredentials } };
+            _confContext.Update(client);
+            _confContext.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
