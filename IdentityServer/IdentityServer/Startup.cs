50785fcc658e9a12899dafbeaafc93b44e8f381c
Vincent <vleclerc@inow.fr>
2020-07-27 14:56:08 +0200

    https://ident.regiomove.de/.well-known/openid-configuration

---
+++ IdentityServer/IdentityServer/Startup.cs
@@ -0,0 +1,91 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace IdentityServer
+{
+    public class Startup
+    {
+        
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var migrationAssemblyName = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+
+            services.AddControllersWithViews();
+
+            var builder = services.AddIdentityServer()
+                .AddConfigurationStore(o =>
+                {
+                    //o.DefaultSchema = "configuration";
+                    o.ConfigureDbContext = b => b.UseSqlServer(Configuration.GetConnectionString("IdentityConnection"),
+                        sql => sql.MigrationsAssembly(migrationAssemblyName));
+                })
+                .AddOperationalStore(o =>
+                {
+                    //o.DefaultSchema = "grant";
+                    o.ConfigureDbContext = b => b.UseSqlServer(Configuration.GetConnectionString("IdentityConnection"),
+                        sql => sql.MigrationsAssembly(migrationAssemblyName));
+                });
+            builder.AddDeveloperSigningCredential();
+
+            /*services.AddAuthentication()
+                .AddOpenIdConnect("oidc", "Formation Identity Server", o =>
+                {
+
+                });*/
+
+
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseIdentityServer();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
